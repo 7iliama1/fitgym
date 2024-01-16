b003007fe967a399f2bb1d99fc8296d31650d816
Ruslan Niazbaev <7iliama1@gmail.com>
2024-01-16 23:00:46 +0200

    Benefit page

---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,159 @@
+import ActionButton from "@/shared/ActionButton";
+import HText from "@/shared/HText";
+import { BenefitType, SelectedPage } from "@/shared/types";
+import {
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
+import Benefit from "./Benefit";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
+      "Explore our state-of-the-art facilities designed to elevate your gym experience.Immerse yourself in cutting-edge equipment and modern spaces crafted to inspire and support your fitness journey.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Choose from a diverse array of classes tailored to meet your fitness goals. With hundreds of options available, you can enjoy a dynamic and personalized workout experience that suits your preferences and schedule.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Train with our expert and pro trainers dedicated to guiding you on your fitness journey. Benefit from personalized workouts, expert advice, and motivation to achieve your fitness goals under the guidance of seasoned professionals.",
+  },
+];
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Benefits = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        {/* HEADER */}
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>MORE THAN JUST A GYM.</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+        </motion.div>
+
+        {/* BENEFITS */}
+        <motion.div
+          className="mt-5 items-center justify-between gap-8 md:flex"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </motion.div>
+
+        {/* GRAPHICS AND DESCRIPTION */}
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          {/* GRAPHIC */}
+          <img
+            className="mx-auto"
+            alt="benefits-page-graphic"
+            src={BenefitsPageGraphic}
+          />
+
+          {/* DESCRIPTION */}
+          <div>
+            {/* TITLE */}
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <HText>
+                    MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500">FIT</span>
+                  </HText>
+                </motion.div>
+              </div>
+            </div>
+
+            {/* DESCRIPT */}
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p className="my-5">
+                Embark on a fitness journey with us and join the millions of individuals who have achieved remarkable success. 
+                From significant weight loss to enhanced strength, our community shares inspiring stories of transformation and overall well-being.
+              </p>
+              <p className="mb-5">
+                Experience more than just a gym; become part of a supportive fitness family. 
+                With a variety of workout options and cutting-edge equipment, our community thrives on diverse achievements. 
+                Join us to celebrate milestones and embrace holistic well-being.
+              </p>
+            </motion.div>
+
+            {/* BUTTON */}
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Benefits;
