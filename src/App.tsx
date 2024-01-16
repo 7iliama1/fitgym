5830840658ba45981e743a827aa21d5f619f10c0
Ruslan Niazbaev <7iliama1@gmail.com>
2024-01-17 00:16:01 +0200

    ContactUs page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Home from "@/scenes/home";
 import OurClasses from "@/scenes/ourClasses";
 import Benefits from "@/scenes/benefits";
 import ContactUs from "@/scenes/contactUs";
-//import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -41,6 +40,3 @@ function App() {
 }
 
 export default App;
-
-
-      //<Footer />
